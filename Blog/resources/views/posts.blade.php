954e1f639d975fd502dfc177fbbf03cf0ff7f88a
Sam Wolters <s.wolters9@icloud.com>
2019-05-25 12:11:42 +0200

    fix

--- Blog/resources/views/posts.blade.php
+++ Blog/resources/views/posts.blade.php
@@ -7,13 +7,12 @@
         <div class="row d-flex justify-content-center">
             @foreach ($posts as $post)
             <div class="card m-2 bg-info" style="width: 18rem;">
-                <img src="{{ "/images/uploads/posts/" . $post->image }}" class="card-img-top image mx-auto d-block" alt="...">
+                <a href="{{ route('show', [$post]) }}">
+                    <img src="{{ "../images/uploads/posts/" . $post->image }}" class="card-img-top image mx-auto d-block" alt="..."></a>
                 <div class="card-body pb-2">
                     <h5 class="card-title">{{ $post->title }}</h5>
                     <div class="card-text">{!! str_limit($post->body, $limit = 180, $end = '...') !!}</div>
                     <div><small>Written on {{$post->created_at}}</small></div>
-                    <a href="posts/{{$post->id}}/edit" class="btn btn-primary">Edit</a>
-                    <span class="btn btn-warning align-middle float-right">{{ $post->likes }}</span>
                 </div>
             </div>
             @endforeach
--- Blog/routes/web.php
+++ Blog/routes/web.php
@@ -10,19 +10,10 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
 Auth::routes();
 
 Route::resource('/user/posts', 'PostsController');
-
-//Route::resource('/post', 'HomeController');
 Route::get('/user/posts/{post}', 'PostsController@show')->name('posts');
-
-//Route::get('/', 'HomeController@index');
 Route::get('/posts', 'HomeController@list')->name('list');
 Route::get('/', 'HomeController@index')->name('index');
 Route::get('/posts/{post}', 'HomeController@show')->name('show');
